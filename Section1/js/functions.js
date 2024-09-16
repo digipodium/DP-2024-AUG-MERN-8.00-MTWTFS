84b5a319d2f8823bf437647dbc1eb1fe8a323d8c
Areeba Shakeel <shakeelareeba02@gmail.com>
2024-09-16 10:59:44 +0530

    arrow function

--- Section1/js/functions.js
+++ Section1/js/functions.js
@@ -22,3 +22,56 @@ function greet1(name){
     console.log("Hello", name)
 }
 greet1("John")
+
+// function without return statement
+function addNum(a,b){
+    console.log(a+b)
+}
+num = addNum(10,20)
+console.log(num)
+
+// function with return statement
+function addNum1(a,b){
+    return a+b
+}
+num1 = addNum1(10,20)
+console.log(num1)
+num2 = addNum1(30,40)
+console.log(num2)
+
+console.log("---------------------------")
+
+function square(n){
+    return n**2
+}       
+console.log(square(3))
+
+console.log("---------------------------")
+
+function myFunc(num,pow){
+    return num**pow
+}
+console.log(myFunc(3,5))
+
+console.log("-----------Arrow Function----------------")
+
+// syntax for arrow function
+// const functionName = () => {
+//     // code to be executed
+// }
+
+const double = (n) => n*2
+console.log(double(5))
+
+console.log("---------------------------")
+
+const evenOdd = () => {
+    for (let i=0; i<=10; i++){
+        if (i%2==0){
+            console.log(i, "is even")
+        } else {
+            console.log(i, "is odd")
+        }
+    }
+}
+evenOdd()
