5c5447b63b56600e2af9ae4cc9c515fc2a850f2d
Areeba Shakeel <shakeelareeba02@gmail.com>
2024-09-26 08:56:18 +0530

    event

---
+++ frontend/src/app/eventHandling/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const EventHandling = () => {
+  return (
+    <div>
+      <h1 className='text-center font-bold text-4xl'>Event Handling</h1>
+    </div>
+  )
+}
+
+export default EventHandling
